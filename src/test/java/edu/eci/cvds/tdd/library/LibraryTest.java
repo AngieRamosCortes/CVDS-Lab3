fd2a19f7584e1980ff2d8c1f6b9d8b427a8ed045
angie <angieram1305@gmail.com>
2025-02-06 22:05:14 -0500

    Laboratorio 3. Cambios hasta la creación del método addBook

--- src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
+++ src/test/java/edu/eci/cvds/tdd/library/LibraryTest.java
@@ -1,10 +1,6 @@
 package edu.eci.cvds.tdd.library;
 
 import edu.eci.cvds.tdd.library.book.Book;
-import edu.eci.cvds.tdd.library.loan.Loan;
-import edu.eci.cvds.tdd.library.loan.LoanStatus;
-import edu.eci.cvds.tdd.library.user.User;
-
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -31,44 +27,4 @@ public class LibraryTest {
         assertFalse(library.addBook(null), "Adding a null book should return false.");
     }
 
-    @Test
-    public void shouldLoanABookSuccessfully() {
-        Library library = new Library();
-        User user = new User();
-        user.setId("123");
-        user.setName("John Doe");
-        library.addUser(user);
-        Book book = new Book("Title1", "Author1", "ISBN1");
-        library.addBook(book);
-
-        Loan loan = library.loanABook("123", "ISBN1");
-        assertNotNull(loan, "The loan should be created successfully.");
-        assertEquals(LoanStatus.ACTIVE, loan.getStatus());
-    }
-
-    @Test
-    public void shouldNotLoanIfBookNotAvailable() {
-        Library library = new Library();
-        User user = new User();
-        user.setId("123");
-        library.addUser(user);
-
-        Loan loan = library.loanABook("123", "ISBN1");
-        assertNull(loan, "The loan should not be created if the book is not available.");
-    }
-
-    @Test
-    public void shouldNotLoanIfUserAlreadyHasBook() {
-        Library library = new Library();
-        User user = new User();
-        user.setId("123");
-        library.addUser(user);
-        Book book = new Book("Title1", "Author1", "ISBN1");
-        library.addBook(book);
-        library.loanABook("123", "ISBN1");
-
-        Loan secondLoan = library.loanABook("123", "ISBN1");
-        assertNull(secondLoan,
-                "The loan should not be created if the user already has an active loan for the same book.");
-    }
 }
